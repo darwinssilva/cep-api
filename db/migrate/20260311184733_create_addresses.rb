dad00fc2d8165e7dd7553cfb6901d37fe501726b
Darwin Silva <darwins.silva.ds@gmail.com>
2026-03-11 15:54:26 -0300

    feat: Create Address model and migration

---
+++ db/migrate/20260311184733_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :addresses do |t|
+      t.string :cep
+      t.string :state
+      t.string :city
+      t.string :neighborhood
+      t.string :street
+      t.decimal :longitude
+      t.decimal :latitude
+
+      t.timestamps
+    end
+  end
+end
